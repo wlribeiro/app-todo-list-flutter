8ccbe74152815d491bea254cc788d56dc4cee07f
will <rybeyro93@gmail.com>
2022-01-05 14:47:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,166 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _toDoList = [];
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
+
+  final _toDoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((value) {
+        setState(() {
+          _toDoList = json.decode(value);
+        });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("lista de tarefas"),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: InputDecoration(
+                        labelText: "Nova tarefa",
+                        labelStyle: TextStyle(color: Colors.deepPurple)),
+                  ),
+                ),
+                RaisedButton(
+                    color: Colors.deepPurple,
+                    child: Text("ADD"),
+                    textColor: Colors.white,
+                    onPressed: _addToDo)
+              ],
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem)
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildItem(context, index){
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+              _toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ), onChanged: (bool? value) {
+        setState(() {
+          _toDoList[index]["ok"] = value;
+          _saveData();
+        });
+      },
+
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+          
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} foi removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: (){
+               setState(() {
+                 _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                 _saveData();
+               });
+              },
+            ),
+            duration: Duration(seconds: 5),
+          );
+          
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+    /*
+    return
+     */
+  }
+
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return "";
+    }
+  }
+}
